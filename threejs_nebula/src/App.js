0af48ef8264599b06d14d16ed73e98a4b022fa04
Michael Perez <mperez6473@gmail.com>
2020-12-15 10:35:45 -0600

    Start of camera and scene

--- threejs_nebula/src/App.js
+++ threejs_nebula/src/App.js
@@ -1,6 +1,24 @@
 import "./App.css";
 import {
 function App() {
+  // Set up, main points: SCENE CAMERA RENDERER
+  let scene, camera, renderer;
+  // Make a new scene and camera
+  scene = new Scene();
+  camera = new PerspectiveCamera(
+    // FOV
+    80,
+    // Aspect ratio
+    window.innerWidth / window.innerHeight,
+    // near clipping plane
+    1,
+    // far clipping plane
+    1000
+  );
+  
+  // Where the camera is located
+  camera.position.z = 10;
+
   return null;
 }
 
